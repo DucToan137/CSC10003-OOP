1755e59c8125460e2ec131d33bf819406155f75e
Nguyen Duc Toan <117813928+DucToan137@users.noreply.github.com>
2024-05-23 01:09:05 +0700

    Add files via upload

---
+++ Demo1705-FractionParse/DemoFractionParse/main.cpp
@@ -0,0 +1,342 @@
+﻿#include<iostream>
+#include<string>
+#include<tuple>
+#include<sstream>
+#include<regex>
+#include<vector>
+#include<memory>
+#include<fstream>
+
+using namespace std;
+
+class Integer {
+public:
+	/// <summary>
+	/// Chuyển chuỗi sang số nguyên
+	/// Error code:
+	/// 0: Thành công
+	/// 1: Lỗi chuỗi không đúng định dạng
+	/// 2: Không có giá trị đầu vào
+	/// 3: Miền giá trị không đúng
+	/// </summary>
+	/// <param name="val">Chuỗi</param>
+	/// <returns></returns>
+	static tuple<bool, int, string, int> parse(string val) 
+	{
+		bool successfull = true;
+		int errorCode = 0;
+		string message = "";
+		int number = 0;
+
+		if (val.size() == 0)
+		{
+			errorCode = 3;
+			message = "Input string cannot be empty";
+		}
+
+		try {
+			number= stoi(val);
+		} 
+		catch (exception ex) {
+			successfull = false;
+			errorCode = 1;
+			message = ex.what();
+		}
+
+		auto result = make_tuple(successfull, errorCode, message, number);
+
+		return result; 
+	}
+
+
+	// Ưu: Luồng xử lí không bị bẽ gãy đột ngột khi có exception
+	// Nhược: 
+	//	1. Không thể biết được lỗi xảy ra ở đâu
+	//	2. Không có cách thông báo cho người dùng
+	// 	3. Giá trị mặc định đội khi không nên có
+	static bool tryParse(string val, int& result)
+	{
+		bool succesfull = false;
+
+		try
+		{
+			result = stoi(val);
+			succesfull = true;
+		}
+		catch (exception ex)
+		{
+			succesfull = false;
+		}
+		return succesfull;
+	}
+};
+
+
+class StringHelper {
+public:
+	static vector<string> split(string line, string separator)
+	{
+		vector<string> result;
+
+		int startPostion = 0;
+		size_t foundPostion = line.find(separator, startPostion);
+
+		while(foundPostion != string::npos)
+		{
+			string token = line.substr(startPostion, foundPostion - startPostion);
+			result.push_back(token);
+
+			startPostion = foundPostion + separator.size();
+			foundPostion = line.find(separator, startPostion);
+
+		}
+
+		string token = line.substr(startPostion, line.size() - startPostion);
+		result.push_back(token);
+
+		return result;
+	}
+};
+
+
+class Fraction { // Data transfer object
+private:
+	int _num;
+	int _den;
+public:
+	int Numerator() { return _num; }
+	void setNumerator(int value) { _num = value; }
+	int Denominator() { return _den; }
+	void setDenominator(int value) { _den = value; }
+public:
+	Fraction() {
+		_num = 0;
+		_den = 0;
+	}
+	Fraction(int num, int den) {
+		_num = num;
+		_den = den;
+	}
+	~Fraction()
+	{
+		cout << "Fraction destructor\n";
+	}
+public:
+	
+	string toString() {
+		stringstream ss;
+		ss << _num << "/" << _den;
+		string result = ss.str();
+		return result;
+	}
+
+	// Không nên viết như thế này
+	void input() {
+		cout << "Nhap tu: ";
+		cin>> _num;
+		cout<<"Nhap mau: ";
+		cin >> _den;
+	}
+
+
+	// Không nên
+	// 1.Truy cập đến giao diện trong lớp DTO (vi phạm nguyên tắc 3 lớp)
+	// 2.friend phá vỡ nguyên tắc hướng đối tượng mà mình học chỉ để phục vụ cú pháp ngôn ngữ
+	friend istream& operator>>(istream& is, Fraction& f) {
+		cout << "Nhap tu: ";
+		is >> f._num;
+		cout << "Nhap mau: ";
+		is >> f._den;
+		return is;
+	}
+
+	/// <summary>
+	/// Chuyển chuỗi sang phân số
+	/// </summary>
+	/// <param name="line">Phan6 số dưới định dạng Tu/Mau</param>
+	/// <returns>
+	/// 0: Thành công
+	/// 1: Không có dữ liệu(chuỗi rỗng)
+	/// 2: Chuỗi không đúng định dạng
+	/// 3: Không đúng miền giá trị(mẫu = 0)
+	/// </returns>
+	static tuple<bool, int, string, shared_ptr<Fraction>> parse(string line) {
+		bool successfull = false;
+		int errorCode = 0;
+		string message = "";
+		shared_ptr<Fraction> f = nullptr;
+
+		if (line.size() == 0)
+		{
+			successfull = false;
+			errorCode = 1;
+			message = "Input string cannot be empty";
+		}
+		else
+		{
+			string pattern = "\\d+\\/\\d+";
+			bool isMatched = regex_match(line, regex(pattern));
+
+			if (!isMatched)
+			{
+				//successfull = false;
+				errorCode = 2;
+				message = "Invalid fraction format";
+			}
+			else
+			{
+				// Tách chuỗi thành 2 phần dựa vào dấu /
+				 const string separator = "/";
+				 auto tokens = StringHelper::split(line, separator);
+				int num = stoi(tokens[0]);
+				int den = stoi(tokens[1]);
+
+				if (den == 0)
+				{
+					//successfull = false;
+					errorCode = 3;
+					message = "Denominator cannot be 0";
+				}
+				else
+				{
+					successfull = true;
+					// f = new Fraction(num, den);
+					f = make_shared<Fraction>(num, den);
+				}
+			}
+		}
+
+
+		auto result = make_tuple(successfull, errorCode, message, f);
+		return result;
+	}
+	
+	
+};
+
+int main() {
+
+	// Thao tác với tuple
+	/*auto point = make_tuple(1, 2);
+	get<0>(point) = 99;
+
+	cout <<get<0>(point) << ",  " << get<1>(point) << "\n";
+
+	int x;
+	int y;
+	tie(x, y) = point;
+	x = 101;
+
+	cout << x << ", " << y << "\n";
+	cout << get<0>(point) << ",  " << get<1>(point) << "\n";*/
+
+	// 1.Chuyển chuỗi sang số nguyên
+
+	/*string val = "hello";
+	bool successfull = false;
+	do {
+
+		cout << "Please enter an integer: ";
+		string line = "";
+
+		int number = 0;
+		getline(cin, line);
+
+		successfull = Integer::tryParse(line, number);
+		if (successfull)
+		{
+			int result = number + 2;
+			cout << result << "\n";
+		}
+		else
+		{
+			cout << "Invalid input. Please try again.\n";
+		}
+	} while (!successfull);*/
+
+	bool successful = false;
+	int errorCode = 0;
+	string message = "";
+	int number = 0;
+	// Con trỏ thông minh
+	shared_ptr<Fraction> f; // reference counting: đếm tham chiếu
+
+	
+	/*do {
+
+		cout << "Please enter an integer: ";
+		string line = "";
+		
+		getline(cin, line);
+		int number = 0;
+
+		tie(successful, errorCode, message, number) = Integer::parse(line);
+
+		if (successful)
+		{
+			int result = number + 2;
+			cout << result << "\n";
+		}
+		else
+		{
+			cout << "Cannot converte to integer. Reason: " << message << "\n";
+			cout << "Please try again.\n";
+		}
+	} while (!successful);*/
+
+
+	/*do {
+		cout << "Please enter a fraction: ";
+		string line = "";
+		getline(cin, line);
+
+		tie(successful, errorCode, message, f) = Fraction::parse(line);
+
+		if (successful)
+		{
+			cout << f->toString() << "\n";
+		}
+		else
+		{
+			cout << "Cannot converte to fraction. Reason: " << message << "\n";
+			cout << "Please try again.\n";
+		}
+	} while (!successful);
+	*/
+
+
+	const string filepath = "data.txt";
+	ifstream reader;
+	reader.open(filepath);
+
+	vector<shared_ptr<Fraction>> a;
+
+
+	while (!reader.eof())
+	{
+		string line;
+		getline(reader, line);
+
+		tie(successful, errorCode, message, f) = Fraction::parse(line);
+
+		a.push_back(f);
+
+		
+	}
+	
+	ofstream writer;
+	writer.open("output.txt");
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		writer << a[i]->toString() << "\n";
+	}
+	//cout << "\n";
+
+
+
+
+	
+	//delete f; // không dùng con trỏ thông minh phải hủy thủ công
+	return 0;
+}
---
+++ Demo1805-Template_Singleton/demo18_5_24/main.cpp
@@ -0,0 +1,317 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<memory>
+
+using namespace std;
+
+// Forward declaration
+class Fraction;
+template<class MyClass>
+class DynamicArray;
+class IntegerRandomizer;
+
+class Fraction {
+private:
+	int _num;
+	int _den;
+public:
+	int Numerator() { return _num; }
+	void setNumerator(int num) { _num = num; }
+	int Denominator() { return _den; }
+	void setDenominator(int den) { _den = den; }
+public:
+	Fraction();
+	Fraction(int num, int den);
+
+	string toString();
+
+	// Copy constructor
+	Fraction(const Fraction& other);
+
+	const Fraction& operator=(const Fraction& other);
+
+	// Copy assignment operator
+};
+
+template<class MyClass>
+class DynamicArray {
+private:
+	//int* _a = nullptr;
+	MyClass *_a = nullptr;
+	int _maxSize;
+	int _currentSize;
+	const int _length = 10;
+public:
+	DynamicArray() {
+		_a = new MyClass[_length];
+		_maxSize = _length;
+		_currentSize = 0;
+	}
+
+	DynamicArray(const DynamicArray& other) {
+		_maxSize = other._maxSize;
+		_currentSize = other._currentSize;
+		//_a = new int[_maxSize];
+		_a = new MyClass[_maxSize];
+		
+		for (int i = 0; i < _currentSize; i++) {
+			_a[i] = other._a[i];
+		}
+	}
+
+	const DynamicArray& operator=(const DynamicArray& other) {
+		_maxSize = other._maxSize;
+		_currentSize = other._currentSize;
+		//_a = new int[_maxSize];
+		_a = new MyClass[_maxSize];
+
+		for (int i = 0; i < _currentSize; i++) {
+			_a[i] = other._a[i];
+		}
+
+		return other;
+	}
+
+	~DynamicArray() {
+		if (_a != nullptr)
+		{
+			delete[] _a;
+		}
+	}
+
+	MyClass getAt(int index) {
+		if (index < _currentSize && index >= 0)
+		{
+			return _a[index];
+		}
+		else
+		{
+			throw new exception("Index out of bound");
+		}
+	}
+
+	MyClass operator[] (int index) {
+		return getAt(index);
+	}
+
+	int size() {
+		return _currentSize;
+	}
+
+	void push_back(MyClass value) {
+		if (_currentSize >= _maxSize)
+		{
+			// Cấp phát vùng nhớ mới 
+			_maxSize = _maxSize + _length;
+			//int * temp = new int[_maxSize];
+			MyClass* temp = new MyClass[_maxSize];
+
+			// Di chuyển dữ liệu cũ qua
+			for (int i = 0; i < _currentSize; i++) {
+				temp[i] = _a[i];
+			}
+
+			// Giải phóng vùng nhớ cũ
+			delete[] _a;
+
+			// Giao quyền quản lí lại cho _a
+			_a = temp;
+		}
+		_a[_currentSize] = value;
+		_currentSize++;
+	}
+
+	void removeAt(int index) {
+		if (index < _currentSize && index >= 0)
+		{
+			for (int i = index + 1; i < _currentSize; i++)
+			{
+				_a[i - 1] = _a[i];
+			}
+			_currentSize--;
+		}
+		else
+		{
+			throw new exception("Index out of bound");
+		}
+	}
+};
+
+// Thiết kế lớp kiểu Singleton
+class IntegerRandomizer {
+private:
+	inline static shared_ptr<IntegerRandomizer> _instance = nullptr; 
+	// Nếu không inline thì cần IntegerRandomizer* IntegerRandomizer::_instance = nullptr;
+	// Dùng inline để gộp thành 1 dòng. Vừa khai báo vừa khởi tạo
+	
+	IntegerRandomizer() {
+		cout << "Ham tao bo sinh so ngau nhien\n"; // Chứng minh khởi tạo 1 lần
+		srand(time(NULL));
+	}
+
+	
+public:
+
+	~IntegerRandomizer() {
+		cout << "Destructor of Integer\n";
+	}
+
+	static shared_ptr<IntegerRandomizer> instance() {
+		if (_instance == nullptr)
+		{
+			_instance = shared_ptr<IntegerRandomizer>(new IntegerRandomizer());
+		}
+		return _instance;
+	}
+
+	int next() {
+		return rand();
+	}
+
+	int next(int ceiling) {
+		return rand() % ceiling;
+	}	
+};
+
+class FractionRandomizer {
+public:
+
+	Fraction next() {
+		int num = IntegerRandomizer::instance()->next(100);
+		int den = IntegerRandomizer::instance()->next(100) + 1;
+
+		Fraction result(num, den);
+
+		return result;
+	}
+};
+
+class Dice {
+private:
+	int _faces = 6;
+public:
+	Dice() {};
+	
+	Dice(int faces) { _faces = faces; }
+	
+	int faces() { return _faces; }
+
+	int roll() {
+		return IntegerRandomizer::instance()->next(_faces) + 1;
+	}
+};
+
+Fraction::Fraction() {
+	_num = 0;
+	_den = 1;
+}
+
+Fraction::Fraction(int num, int den) {
+	_num = num;
+	_den = den;
+}
+
+string Fraction::toString() {
+	stringstream builder;
+	builder << _num << "/" << _den;
+	string result = builder.str();
+
+	return result;
+}
+
+Fraction::Fraction(const Fraction& other) {
+	//cout << "Copy constructor\n";
+	this->_num = other._num;
+	this->_den = other._den;
+}
+
+const Fraction& Fraction::operator=(const Fraction& other) {
+	//cout <<"Operator= \n";
+	this->_num = other._num;
+	this->_den = other._den;
+
+	return other;
+	//return *this;
+
+}	// this -> Fraction* || *this -> Fraction
+
+int main() {
+
+	//Fraction start(1, 2);
+	////Fraction clone(start);
+
+	////Fraction end = start = clone; // a = b = c = d
+
+	//Fraction x = start; // Constructor Fraction, operator= ===> Copy constructor
+
+	//cout << start.toString() << endl;
+	//cout << x.toString() << endl;
+
+	//DynamicArray a;
+	//IntegerRandomizer rng;
+
+	//for (int i = 0; i < 20; i++) {
+	//	a.push_back(rng.next());
+	//}
+
+	//for (int i = 0; i < a.size(); i++) {
+	//	cout << a[i] << " ";
+	//}
+
+	//cout << "\n";
+
+
+	////DynamicArray b(a); // Cần hàm tạo sao chép nếu không => lỗi
+
+	//DynamicArray b;
+	//b = a;
+	//
+
+	//for (int i = b.size() - 1; i >= 0; i--) {
+	//	if (b[i] % 2 == 0)
+	//	{
+	//		b.removeAt(i);
+	//	}
+	//}
+
+	//cout <<"Mang sau khi loc so chan: ";
+	//for (int i = 0; i < b.size(); i++) {
+	//	cout << b[i] << " ";
+	//}
+
+	// Phát sinh ngấu nhiên 20 phân số và in ra màn hình
+	/*DynamicArray<Fraction> a;
+	FractionRandomizer rng;
+
+	for (int i = 0; i < 20; i++) {
+		a.push_back(rng.next());
+	}
+
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i].toString() << "\n";
+	}	*/
+	Dice d1;
+	Dice d2;
+	vector<int> a1;
+	vector<int> a2;
+
+	for (int i = 0; i < 10; i++) {
+		a1.push_back(d1.roll());
+		a2.push_back(d2.roll());
+	}
+
+	for (int i = 0; i < a1.size(); i++)
+	{
+		cout << a1[i] << " ";
+	}
+	cout << "\n";
+	for (int i = 0; i < a2.size(); i++)
+	{
+		cout << a2[i] << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
